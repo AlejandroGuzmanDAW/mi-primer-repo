e0b1bc57605fa873a2b83279e406f17c58147089
AlejandroGuzmanDAW <carlosalejandro.montano@plazacastilla.salesianas.org>
2025-04-30 12:50:28 +0200

    Update PruebaGitHub.java

--- Prueba/src/ejemplo/PruebaGitHub.java
+++ Prueba/src/ejemplo/PruebaGitHub.java
@@ -2,7 +2,7 @@ package ejemplo;
 
 public class PruebaGitHub {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//comentario prueba
 		// TODO Auto-generated method stub
 
 	}
